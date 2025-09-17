7a85db6e8aeaf506d3e5b620415d4811ad1e374a
virtual-analytics <virtual.analysts.projects@gmail.com>
2025-09-17 16:46:52 +0300

    first commit

--- app/templatetags/custom_filters.py
+++ app/templatetags/custom_filters.py
@@ -5,3 +5,23 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def get_item(dictionary, key):
+    """Return the value from a dict for the given key."""
+    if isinstance(dictionary, dict):
+        return dictionary.get(key)
+    return None
+
+
+@register.filter
+def get_item(dictionary, key):
+    try:
+        return dictionary.get(key) if hasattr(dictionary, 'get') else dictionary[key]
+    except Exception:
+        return ''
+
+@register.filter
+def get_item(dictionary, key):
+    """Get a dictionary item by key"""
+    return dictionary.get(key, "")
