9c3c640a8eb47fa10d45ff4c56948808021362db
kipronopato <144670406+kipronopato@users.noreply.github.com>
2025-11-04 01:25:54 -0800

    commit

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ CSRF_COOKIE_SECURE = True
 DB_LIVE = os.getenv("DB_LIVE", "False").lower() in ["true", "1", "yes"]
 
 # Allowed hosts
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # CSRF trusted origins
 CSRF_TRUSTED_ORIGINS = [
